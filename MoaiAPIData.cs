a9fb58b9905e8c78ac76827c26b58eb246c47dde
Kanbaru <lihaochen910@hotmail.com>
2018-07-01 16:50:09 +0800

    init commit

---
+++ MoaiAPIData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace MoaiAPIDocParser
+{
+    public class MoaiAPIData
+    {
+        public string ClassName;
+        public string Url;
+        public string ClassDesc;
+
+        public Dictionary<string, MoaiClassFunctionData> FunctionDic;
+        public MoaiAPIData()
+        {
+            FunctionDic = new Dictionary<string, MoaiClassFunctionData>();
+        }
+    }
+
+    public class MoaiClassFunctionData
+    {
+        public string FunctionName;
+        public string Code;
+        public List<string> FunctionParams;
+
+        public MoaiClassFunctionData()
+        {
+            FunctionParams = new List<string>();
+        }
+    }
+
+    public class SublimeCompletionsFile
+    {
+        public string scope = "source.luae, text.lua - source - meta.tag, punctuation.definition.tag.begin";
+        public string version = "0.1";
+        public List<CompletionData> completions;
+
+        public SublimeCompletionsFile()
+        {
+            completions = new List<CompletionData>();
+        }
+
+        public class CompletionData
+        {
+            public string trigger;
+            public string contents;
+        }
+    }
+}
